5166ba77e8f70b5f2eebda1ae5d44293fb3f67c7
monoglo <monoglo37@gmail.com>
2020-06-25 20:32:26 +0800

    fix(security): encrypt user password

--- src/main/java/com/rankofmatrix/blog/service/impl/UserAPIServiceImpl.java
+++ src/main/java/com/rankofmatrix/blog/service/impl/UserAPIServiceImpl.java
@@ -1,5 +1,6 @@
 package com.rankofmatrix.blog.service.impl;
 
+import org.jasypt.encryption.StringEncryptor;
 import com.google.common.collect.Lists;
 import com.rankofmatrix.blog.model.User;
 import com.rankofmatrix.blog.repository.UserRepository;
@@ -13,10 +14,16 @@ import java.util.List;
 public class UserAPIServiceImpl implements UserAPIService{
 
     private UserRepository userRepository;
+    private StringEncryptor encryptor;
 
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setEncryptor(StringEncryptor encryptor) {
+        this.encryptor = encryptor;
     }
 
     @Override
@@ -29,7 +36,7 @@ public class UserAPIServiceImpl implements UserAPIService{
         if (userRepository.findByEmail(registerUser.getEmail()) == null) {
             User checkedUser = new User();
             checkedUser.setEmail(registerUser.getEmail());
-            checkedUser.setPassword(registerUser.getPassword());
+            checkedUser.setPassword(encryptor.encrypt(registerUser.getPassword()));
             checkedUser.setNickname(registerUser.getNickname());
             return userRepository.save(checkedUser);
         } else {
@@ -42,7 +49,7 @@ public class UserAPIServiceImpl implements UserAPIService{
         User checkedUser;
         checkedUser =  userRepository.findByEmail(loginUser.getEmail());
         if (checkedUser != null) {
-            if (checkedUser.getPassword().equals(loginUser.getPassword())) {
+            if (encryptor.decrypt(checkedUser.getPassword()).equals(loginUser.getPassword())) {
                 return checkedUser;
             } else {
                 return null;
