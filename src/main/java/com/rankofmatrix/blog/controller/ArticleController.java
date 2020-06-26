a12a51c073c84ceb82e2ed17ff090fd973fb3b8f
monoglo <monoglo37@gmail.com>
2020-06-26 21:54:29 +0800

    style(controller): combine @ResponseBody and @Controller
    Use RestController instead

--- src/main/java/com/rankofmatrix/blog/controller/ArticleController.java
+++ src/main/java/com/rankofmatrix/blog/controller/ArticleController.java
@@ -5,12 +5,11 @@ import com.rankofmatrix.blog.model.JsonResponse;
 import com.rankofmatrix.blog.service.impl.ArticleAPIServiceImpl;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path = "/articles")
 @Api(tags = {"Article API"})
 @SwaggerDefinition(tags = {
@@ -28,7 +27,7 @@ public class ArticleController {
     // 获取所有文章(包含被删除)
     @GetMapping(path = "/")
     @ApiOperation("获取所有文章(包含被删除)")
-    public @ResponseBody JsonResponse getAllArticle() {
+    public JsonResponse getAllArticle() {
         List<Article> resultArticles = articleAPIService.getAllArticle();
         if (resultArticles.size() > 0) {
             return new JsonResponse(200, "Get all articles successfully", resultArticles);
@@ -40,7 +39,7 @@ public class ArticleController {
     // 获取所有可见的文章
     @GetMapping(path = "/visible")
     @ApiOperation("获取所有可见的文章")
-    public @ResponseBody JsonResponse getAllArticleVisible() {
+    public JsonResponse getAllArticleVisible() {
         List<Article> resultArticles = articleAPIService.getAllArticleVisible();
         if (resultArticles.size() > 0) {
             return new JsonResponse(200, "Get all articles successfully", resultArticles);
@@ -52,7 +51,7 @@ public class ArticleController {
     // 获取所有被删除的文章
     @GetMapping(path = "/deleted")
     @ApiOperation("获取所有被删除的文章")
-    public @ResponseBody JsonResponse getAllArticleDeleted() {
+    public JsonResponse getAllArticleDeleted() {
         List<Article> resultArticles = articleAPIService.getAllArticleDeleted();
         if (resultArticles.size() > 0) {
             return new JsonResponse(200, "Get all articles successfully", resultArticles);
@@ -65,7 +64,7 @@ public class ArticleController {
     @GetMapping(path = "/aid/{aid}")
     @ApiOperation("获取某一ID的文章(包含被删除)")
     @ApiImplicitParam(name = "aid", value = "文章ID", required = true, dataType = "Int")
-    public @ResponseBody JsonResponse getArticleByAid(@PathVariable(value = "aid") Integer aid) {
+    public JsonResponse getArticleByAid(@PathVariable(value = "aid") Integer aid) {
         Article resultArticle = articleAPIService.getArticleByAid(aid);
         if (resultArticle != null) {
             return new JsonResponse(200, "Get article successfully", resultArticle);
@@ -78,7 +77,7 @@ public class ArticleController {
     @GetMapping(path = "/uid/{uid}")
     @ApiOperation("获取某一作者的所有文章(不带正文)")
     @ApiImplicitParam(name = "uid", value = "作者ID", required = true, dataType = "Int")
-    public @ResponseBody JsonResponse getArticleWithoutTextByUid(@PathVariable(value = "uid") Integer uid) {
+    public JsonResponse getArticleWithoutTextByUid(@PathVariable(value = "uid") Integer uid) {
         List<Article> resultArticles = articleAPIService.getArticleWithoutTextByUid(uid);
         if (resultArticles.size() > 0) {
             return new JsonResponse(200, "Get author's all articles successfully", resultArticles);
@@ -91,7 +90,7 @@ public class ArticleController {
     @GetMapping(path = "/titleKey/{titleKey}")
     @ApiOperation("检索标题中包含关键字的所有文章(不带正文)")
     @ApiImplicitParam(name = "titleKey", value = "标题检索关键词", required = true, dataType = "String")
-    public @ResponseBody JsonResponse selectArticleWithoutTextByTitleKey(@PathVariable(value = "titleKey") String titleKey) {
+    public JsonResponse selectArticleWithoutTextByTitleKey(@PathVariable(value = "titleKey") String titleKey) {
         List<Article> resultArticles = articleAPIService.selectArticleWithoutTextByTitleKey(titleKey);
         if (resultArticles.size() > 0) {
             return new JsonResponse(200, "Search articles successfully", resultArticles);
@@ -104,7 +103,7 @@ public class ArticleController {
     @GetMapping(path = "/key/{key}")
     @ApiOperation("检索模糊搜索带有关键字的所有文章(不带正文)")
     @ApiImplicitParam(name = "key", value = "全文检索关键词", required = true, dataType = "String")
-    public @ResponseBody JsonResponse selectArticleWithoutTextByKey(@PathVariable(value = "key") String key) {
+    public JsonResponse selectArticleWithoutTextByKey(@PathVariable(value = "key") String key) {
         List<Article> resultArticles = articleAPIService.selectArticleWithoutTextByKey(key);
         if (resultArticles.size() > 0) {
             return new JsonResponse(200, "Search articles successfully", resultArticles);
@@ -117,7 +116,7 @@ public class ArticleController {
     @PostMapping(path = "/")
     @ApiOperation("创建新的文章")
     @ApiImplicitParam(name = "createArticle", value = "将要创建的文章信息(标题必要)", required = true, dataType = "Article")
-    public @ResponseBody JsonResponse createArticleByArticle(@RequestBody Article createArticle) {
+    public JsonResponse createArticleByArticle(@RequestBody Article createArticle) {
         Article createdArticle = articleAPIService.createArticleByArticle(createArticle);
         if (createdArticle != null) {
             return new JsonResponse(201, "Create article successfully", createdArticle);
@@ -130,7 +129,7 @@ public class ArticleController {
     @PutMapping(path = "/")
     @ApiOperation("修改某一ID的文章(包含被删除)")
     @ApiImplicitParam(name = "updateArticle", value = "将要修改的文章信息", required = true, dataType = "Article")
-    public @ResponseBody JsonResponse modifyArticleByArticle(@RequestBody Article updateArticle) {
+    public JsonResponse modifyArticleByArticle(@RequestBody Article updateArticle) {
         Article modifiedArticle = articleAPIService.modifyArticleByArticle(updateArticle);
         if (modifiedArticle != null) {
             return new JsonResponse(200, "Update article successfully", modifiedArticle);
@@ -141,13 +140,13 @@ public class ArticleController {
 
     // TODO 添加标签到某一文章 API
     @PutMapping(path = "/add/tag/archiveId")
-    public @ResponseBody JsonResponse addTagToArticleByAidAndTagId() {
+    public JsonResponse addTagToArticleByAidAndTagId() {
         return null;
     }
 
     // TODO 添加归档到某一文章 API
     @PutMapping(path = "/add/archive/archiveId")
-    public @ResponseBody JsonResponse addArchiveToArticleByAidAndArchiveId() {
+    public JsonResponse addArchiveToArticleByAidAndArchiveId() {
         return null;
     }
 
@@ -155,7 +154,7 @@ public class ArticleController {
     @ApiOperation("删除某一ID的文章")
     @ApiImplicitParam(name = "aid", value = "将要删除的文章ID", required = true, dataType = "Int")
     @DeleteMapping(path = "/aid/{aid}")
-    public @ResponseBody JsonResponse deleteArticleByAid(@PathVariable(value = "aid") Integer aid) {
+    public JsonResponse deleteArticleByAid(@PathVariable(value = "aid") Integer aid) {
         Boolean result = articleAPIService.deleteArticleByAid(aid);
         if (result) {
             return new JsonResponse(200, "Delete article successfully", null);
--- src/main/java/com/rankofmatrix/blog/controller/UserController.java
+++ src/main/java/com/rankofmatrix/blog/controller/UserController.java
@@ -5,13 +5,11 @@ import com.rankofmatrix.blog.model.User;
 import com.rankofmatrix.blog.model.JsonResponse;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.spring.web.json.Json;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path = "/users")
 @Api(tags = {"User API"})
 @SwaggerDefinition(tags = {
@@ -29,7 +27,7 @@ public class UserController {
     @ApiOperation("用户注册接口")
     @ApiImplicitParam(name = "registerUser", value = "将要注册的用户信息（邮箱、密码必要）", required = true, dataType = "User")
     @PostMapping(path = "/register")
-    public @ResponseBody JsonResponse registerUser (@RequestBody User registerUser) {
+    public JsonResponse registerUser (@RequestBody User registerUser) {
         User resultUser = userAPIService.registerUser(registerUser);
         if (resultUser != null) {
             return new JsonResponse(201, "Register successfully", resultUser);
@@ -40,7 +38,7 @@ public class UserController {
 
     @ApiOperation("获取所有用户")
     @GetMapping(path = "/")
-    public @ResponseBody JsonResponse getAllUsers() {
+    public JsonResponse getAllUsers() {
         List<User> resultUsers = userAPIService.getAllUsers();
         if (resultUsers.size() > 0) {
             return new JsonResponse(200, "Get all users successfully", resultUsers);
@@ -52,7 +50,7 @@ public class UserController {
     @ApiOperation("用户登陆接口")
     @PostMapping(path = "/login")
     @ApiImplicitParam(name = "loginUser", value = "将要登陆的用户信息（邮箱、密码必要）", required = true, dataType = "User")
-    public @ResponseBody JsonResponse loginUser(@RequestBody User loginUser) {
+    public JsonResponse loginUser(@RequestBody User loginUser) {
         User resultUser = userAPIService.loginUser(loginUser);
         if (resultUser != null) {
             return new JsonResponse(200, "Login successfully", resultUser);
